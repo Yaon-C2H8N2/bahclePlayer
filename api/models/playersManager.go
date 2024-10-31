66ac2dba57b646f1f2b056e9c3bf45edf82eb21a
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-10-31 22:20:20 +0100

    chore: refacto notification handling

--- api/models/playersManager.go
+++ api/models/playersManager.go
@@ -1,15 +1,12 @@
 package models
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/Yaon-C2H8N2/bahclePlayer/models/songRequests"
 	"github.com/Yaon-C2H8N2/bahclePlayer/models/twitch"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"net/http"
 	"os"
-	"regexp"
 	"sync"
 )
 
@@ -70,76 +67,10 @@ func (pm *PlayersManager) CreatePlayer(c *gin.Context) {
 func (pm *PlayersManager) mainLoop(token string) {
 	conn := pm.clients[token]
 
-	requestManager := songRequests.GetRequestManager()
+	notifcationHandler := twitch.GetNotificationHandler(pm.apiWrapper, token)
 
 	pm.eventSub.OnEvent(token, func(event twitch.NotificationMessage) {
-		eventBytes, _ := json.Marshal(event.Payload.Event)
-
-		switch event.Payload.Subscription.Type {
-		case "channel.channel_points_custom_reward_redemption.add":
-			var redemptionEvent = &twitch.ChannelPointsCustomRewardRedemptionAddEvent{}
-			err := json.Unmarshal(eventBytes, redemptionEvent)
-			if err != nil {
-				fmt.Println("Failed to unmarshal redemption event")
-				break
-			}
-
-			message := redemptionEvent.UserInput
-			youtubeIdRegexp := regexp.MustCompile(`(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/((watch\?v=|embed/|v/|e/|u/\w+/|v=|\?v=)?)([^#&?]{11})`)
-			youtubeId := youtubeIdRegexp.FindString(message)
-			if youtubeId == "" {
-				fmt.Println("Failed to extract youtube id from message")
-				err = pm.apiWrapper.UpdateRedemptionStatus(token, redemptionEvent.Id, redemptionEvent.BroadcasterUserId, redemptionEvent.Reward.Id, "CANCELED")
-				break
-			}
-
-			var songRequest = songRequests.SongRequest{}
-			songRequest.TwitchRedemptionID = redemptionEvent.Id
-			songRequest.TwitchRewardID = redemptionEvent.Reward.Id
-			songRequest.YoutubeID = youtubeId
-			songRequest.Title = ""     //TODO: get video title
-			songRequest.Channel = ""   //TODO: get video channel
-			songRequest.Duration = 0   //TODO: get video duration
-			songRequest.Thumbnail = "" //TODO: get video base64 thumbnail or url
-
-			pollTitle := fmt.Sprintf("Should we play %s by %s?", songRequest.Title, songRequest.Channel)
-			twitchPollId, err := pm.apiWrapper.CreatePoll(token, redemptionEvent.BroadcasterUserId, pollTitle, []string{"Yes", "No"}, 60)
-			songRequest.TwitchPollID = twitchPollId
-			requestManager.AddRequest(songRequest)
-			break
-		case "channel.poll.end":
-			var pollEndEvent = &twitch.ChannelPollEndEvent{}
-			err := json.Unmarshal(eventBytes, pollEndEvent)
-			if err != nil {
-				fmt.Println("Failed to unmarshal poll end event")
-				break
-			}
-
-			songRequest := requestManager.GetRequest(pollEndEvent.Id)
-			if songRequest.TwitchPollID == "" {
-				fmt.Println("Failed to get song request from poll id")
-				break
-			}
-
-			maxVotes := 0
-			maxChoice := ""
-			newStatus := "CANCELED"
-			for _, choice := range pollEndEvent.Choices {
-				if choice.BitsVotes+choice.ChannelPointsVotes > maxVotes {
-					maxVotes = choice.BitsVotes + choice.ChannelPointsVotes
-					maxChoice = choice.Title
-				}
-			}
-			if maxChoice == "Yes" {
-				//TODO: send song request to player if valid
-				newStatus = "FULFILLED"
-			}
-			err = pm.apiWrapper.UpdateRedemptionStatus(token, songRequest.TwitchRedemptionID, pollEndEvent.BroadcasterUserId, songRequest.TwitchRewardID, newStatus)
-			if err != nil {
-				fmt.Println("Failed to update redemption status")
-				break
-			}
-		}
+		notifcationHandler.Handle(&event)
 	})
 
 	if pm.eventSub.IsStarted() {
---
+++ api/models/twitch/notificationHandler.go
@@ -0,0 +1,99 @@
+package twitch
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Yaon-C2H8N2/bahclePlayer/models/songRequests"
+	"regexp"
+)
+
+type NotificationHandler struct {
+	handlers       map[string]func([]byte)
+	apiWrapper     *ApiWrapper
+	requestManager *songRequests.RequestManager
+	token          string
+}
+
+func GetNotificationHandler(apiWrapper *ApiWrapper, token string) *NotificationHandler {
+	handler := &NotificationHandler{
+		handlers:       make(map[string]func([]byte)),
+		apiWrapper:     apiWrapper,
+		requestManager: songRequests.GetRequestManager(),
+		token:          token,
+	}
+
+	handler.handlers["channel.channel_points_custom_reward_redemption.add"] = handler.handleChannelPointsCustomRewardRedemptionAdd
+
+	return handler
+}
+
+func (nh *NotificationHandler) Handle(notification *NotificationMessage) {
+	notificationBytes, _ := json.Marshal(notification)
+	nh.handlers[notification.Payload.Subscription.Type](notificationBytes)
+}
+
+func (nh *NotificationHandler) handleChannelPointsCustomRewardRedemptionAdd(eventBytes []byte) {
+	var redemptionEvent = &ChannelPointsCustomRewardRedemptionAddEvent{}
+	err := json.Unmarshal(eventBytes, redemptionEvent)
+	if err != nil {
+		fmt.Println("Failed to unmarshal redemption event")
+		return
+	}
+
+	message := redemptionEvent.UserInput
+	youtubeIdRegexp := regexp.MustCompile(`(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/((watch\?v=|embed/|v/|e/|u/\w+/|v=|\?v=)?)([^#&?]{11})`)
+	youtubeId := youtubeIdRegexp.FindString(message)
+	if youtubeId == "" {
+		fmt.Println("Failed to extract youtube id from message")
+		err = nh.apiWrapper.UpdateRedemptionStatus(nh.token, redemptionEvent.Id, redemptionEvent.BroadcasterUserId, redemptionEvent.Reward.Id, "CANCELED")
+		return
+	}
+
+	var songRequest = songRequests.SongRequest{}
+	songRequest.TwitchRedemptionID = redemptionEvent.Id
+	songRequest.TwitchRewardID = redemptionEvent.Reward.Id
+	songRequest.YoutubeID = youtubeId
+	songRequest.Title = ""     //TODO: get video title
+	songRequest.Channel = ""   //TODO: get video channel
+	songRequest.Duration = 0   //TODO: get video duration
+	songRequest.Thumbnail = "" //TODO: get video base64 thumbnail or url
+
+	pollTitle := fmt.Sprintf("Should we play %s by %s?", songRequest.Title, songRequest.Channel)
+	twitchPollId, err := nh.apiWrapper.CreatePoll(nh.token, redemptionEvent.BroadcasterUserId, pollTitle, []string{"Yes", "No"}, 60)
+	songRequest.TwitchPollID = twitchPollId
+	nh.requestManager.AddRequest(songRequest)
+}
+
+func (nh *NotificationHandler) handleChannelPollEnd(token string, eventBytes []byte) {
+	var pollEndEvent = &ChannelPollEndEvent{}
+	err := json.Unmarshal(eventBytes, pollEndEvent)
+	if err != nil {
+		fmt.Println("Failed to unmarshal poll end event")
+		return
+	}
+
+	songRequest := nh.requestManager.GetRequest(pollEndEvent.Id)
+	if songRequest.TwitchPollID == "" {
+		fmt.Println("Failed to get song request from poll id")
+		return
+	}
+
+	maxVotes := 0
+	maxChoice := ""
+	newStatus := "CANCELED"
+	for _, choice := range pollEndEvent.Choices {
+		if choice.BitsVotes+choice.ChannelPointsVotes > maxVotes {
+			maxVotes = choice.BitsVotes + choice.ChannelPointsVotes
+			maxChoice = choice.Title
+		}
+	}
+	if maxChoice == "Yes" {
+		//TODO: send song request to player if valid
+		newStatus = "FULFILLED"
+	}
+	err = nh.apiWrapper.UpdateRedemptionStatus(token, songRequest.TwitchRedemptionID, pollEndEvent.BroadcasterUserId, songRequest.TwitchRewardID, newStatus)
+	if err != nil {
+		fmt.Println("Failed to update redemption status")
+		return
+	}
+}
