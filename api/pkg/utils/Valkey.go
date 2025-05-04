43f92d78ebf1e4217f1c701df1d62e0d962a008a
Yoan Dusoleil <y.dusoleil@outlook.com>
2025-05-04 13:33:29 +0200

    fix(wip): fixed valkey connection

--- api/pkg/utils/Valkey.go
+++ api/pkg/utils/Valkey.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 	"os"
 )
@@ -10,12 +11,18 @@ var ValkeyClient *redis.Client
 func InitValkey() {
 	address := os.Getenv("VALKEY_URL")
 	port := os.Getenv("VALKEY_PORT")
+	password := os.Getenv("VALKEY_PASSWORD")
 
 	ValkeyClient = redis.NewClient(&redis.Options{
 		Addr:     address + ":" + port,
-		Password: "",
+		Password: password,
 		DB:       0,
 	})
+
+	_, err := ValkeyClient.Ping(context.Background()).Result()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func GetValkeyClient() *redis.Client {
